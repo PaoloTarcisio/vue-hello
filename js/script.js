a04f56f5fdab19939551a1f04da6db0680a8a0de
PaoloTarcisio <paoloc50@gmail.com>
2023-12-23 16:10:51 +0100

    Aggiunta immagine e classi ad essa

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ createApp({
         }
     }
 }).mount('#app')
+
+createApp({
+    data() {
+        return {
+            link: 'https://images.unsplash.com/photo-1550475966-70af27831597?q=80&w=3571&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            mieClassi: 'classe-immagine'
+        }
+    }
+}).mount('#immagine')
